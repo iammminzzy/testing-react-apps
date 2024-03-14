2146190e0b556d3ea26599c1a184fc11d7c71ced
minjee <minjeeson.dev@gmail.com>
2024-03-14 01:50:36 +0000

    Exercise 1 : simple test with ReactDOM

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -3,33 +3,59 @@
 
 import * as React from 'react'
 import {act} from 'react-dom/test-utils'
-import {createRoot} from 'react-dom/client'
 import Counter from '../../components/counter'
+import {createRoot} from 'react-dom/client'
 
 // NOTE: this is a new requirement in React 18
 // https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
 // Luckily, it's handled for you by React Testing Library :)
+
 global.IS_REACT_ACT_ENVIRONMENT = true
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  // test case 1: render
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+  const message = div.firstChild.querySelector('div')
+  expect(message.textContent).toBe('Current count: 0')
+
+  // test case 2: button click
+  const [decrement, increment] = div.querySelectorAll('button')
+  act(() => decrement.click())
+  expect(message.textContent).toBe('Current count: -1')
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 0')
+})
+
+test('extra credit #1: use dispatchEvent', () => {
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  // test case 1: render
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+  const message = div.firstChild.querySelector('div')
+
+  // test case 2: button click
+  const [decrement, increment] = div.querySelectorAll('button')
+  const clickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  act(() => increment.dispatchEvent(clickEvent))
+  expect(message.textContent).toBe('Current count: 1')
+  act(() => decrement.dispatchEvent(clickEvent))
+  expect(message.textContent).toBe('Current count: 0')
+  act(() => decrement.dispatchEvent(clickEvent))
+  expect(message.textContent).toBe('Current count: -1')
 })
 
 /* eslint no-unused-vars:0 */
